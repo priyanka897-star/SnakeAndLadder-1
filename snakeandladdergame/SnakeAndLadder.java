ca88713c356a88453eafeac0ff18632f62e27b2b
hrushikesh91 <63092387+hrushikesh91@users.noreply.github.com>
2021-09-27 14:41:53 +0530

    Please enter the commit message for your changes. Lines starting ("Added: Deciding Player Moves")

--- snakeandladdergame/SnakeAndLadder.java
+++ snakeandladdergame/SnakeAndLadder.java
@@ -2,9 +2,32 @@ package snakeandladdergame;
 
 public class SnakeAndLadder {
 	public static void main(String[] args) {
-	int startPoint = 0; 
-	System.out.println("start position "+ startPoint);
-	int randomNumber = (int)((Math.random()*10) % 6)+1;
-	System.out.println("random number is:" +randomNumber); 
+	//int startPoint = 0; 
+	//System.out.println("start position "+ startPoint);
+	int dice = (int)((Math.random()*10) % 6);
+	System.out.println("random number is:" +dice);   
+    int position  = (int)(Math.random()*10) % 3;
+    int presentPosition = 70;
+    int ladder = 1;
+    int snake = 2;
+    switch(dice)
+    {
+    case 0: 
+    	System.out.println("No Movement");
+    	break;
+    case 1: 
+    position = ladder;
+    presentPosition +=8;
+    System.out.println("Ladder move forward "+ presentPosition);
+    break;
+    case 2:
+    position = snake;
+    presentPosition -=10;
+    System.out.println("Snakes move backward "+ presentPosition);
+    break;
+    default :
+    presentPosition +=dice;
+    System.out.println("Movement to " +presentPosition );
+    }
 }
 }
