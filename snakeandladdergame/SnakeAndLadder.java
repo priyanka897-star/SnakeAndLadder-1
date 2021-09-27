24afcfaf65b40d75a7793e0f9e53367ffc5b0365
hrushikesh91 <63092387+hrushikesh91@users.noreply.github.com>
2021-09-27 13:17:53 +0530

    Added: Generate random Number

--- snakeandladdergame/SnakeAndLadder.java
+++ snakeandladdergame/SnakeAndLadder.java
@@ -2,7 +2,9 @@ package snakeandladdergame;
 
 public class SnakeAndLadder {
 	public static void main(String[] args) {
-	 int startPoint = 0; 
-	 System.out.println("start position "+ startPoint);
+	int startPoint = 0; 
+	System.out.println("start position "+ startPoint);
+	int randomNumber = (int)((Math.random()*10) % 6)+1;
+	System.out.println("random number is:" +randomNumber); 
 }
 }
