f712ddf9a3dba308931a85cfb08e563f0f77ca8a
hrushikesh91 <63092387+hrushikesh91@users.noreply.github.com>
2021-09-27 13:07:23 +0530

    Added: Start Position

---
+++ snakeandladdergame/SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package snakeandladdergame;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+	 int startPoint = 0; 
+	 System.out.println("start position "+ startPoint);
+}
+}
